ac23f2cbc401fc6da08b7f508a3141be902f8ed2
fob1493 <fob1493@gmail.com>
2018-10-10 23:17:17 +0100

    Make modification to the app

--- routes/cards.js
+++ routes/cards.js
@@ -3,35 +3,35 @@ const router = express.Router();
 const { data } = require('../data/flaskcardData.json');
 const { cards } = data;
 
-router.get('/', (req, res) => {
+router.get( '/', ( req, res ) => {
   const numberOfCards = cards.length;
-  const flashcardId = Math.floor(Math.random() * numberOfCards);
-  res.redirect(`/cards/${flashcardId}`);
+  const flashcardId = Math.floor( Math.random() * numberOfCards );
+  res.redirect( `/cards/${flashcardId}` )
 });
 
 router.get('/:id', (req, res) => {
-  const { side } = req.query;
-  const { id } = req.params;
+    const { side } = req.query;
+    const { id } = req.params;
 
-  if ( !side ) {
-    return res.redirect(`/cards/${id}?side=question`);
-  }
+    if ( !side ) {
+        return res.redirect(`/cards/${id}?side=question`);
+    }
+    const name = req.cookies.username;
+    const text = cards[id][side];
+    const { hint } = cards[id];
+    
+    const templateData = { id, text, name, side };
 
-  const name = req/cookies.username;
-  const text = cards[id][side];
-  const { hint } = cards[id];
-  const templateData = { id, text, name };
+    if ( side === 'question' ) {
+      templateData.hint = hint;
+      templateData.sideToShow = 'answer';
+      templateData.sideToShowDisplay = 'Answer';
+    } else if ( side === 'answer' ) {
+      templateData.sideToShow = 'question';
+      templateData.sideToShowDisplay = 'Question';
+    }
 
-  if (side === 'question' ) {
-    templateData.hint = hint;
-    templateData.sideToShow = 'answer';
-    templateData.sideToShowDisplay = 'Answer';
-  } else if (side === 'answer') {
-    templateData.sideToShow = 'question';
-    templateData.sideToShowDisplay = 'Question';
-  }
-
-  res.render('card', templateData);
+    res.render('card', templateData);
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-// app.use((req, res, next) => {
-//   console.log('Hello');
-//   const err = new Error('Noooo!!!!');
-//   err.status = 500;
-//   next(err);
-// });
-
-// app.use((req, res, next) => {
-//   console.log('World');
-//   next();
-// });
-
 router.get('/', (req, res) => {
-  const name = req.cookies.username;
-  if (name) {
-    res.render('index', { name})
-  } else {
-    res.redirect('/hello');
-  }
+    const name = req.cookies.username;
+    if (name) {
+      res.render('index', { name });
+    } else {
+      res.redirect('/hello');
+    }
 });
 
 router.get('/hello', (req, res) => {
@@ -36,7 +24,7 @@ router.post('/hello', (req, res) => {
   res.redirect('/');
 });
 
-router.post('/clear', (req, res) => {
+router.post('/goodbye', (req, res) => {
   res.clearCookie('username');
   res.redirect('/hello');
 });
